efbb9f66c6b5ef370df55998f408b8a9a5613008
Kevin Green <stuckbykev@gmail.com>
2024-07-23 16:54:48 -0700

    Initial Commit

---
+++ BootyDrop/BootyDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootyDropApp.swift
+//  BootyDrop
+//
+//  Created by Kevin Green on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BootyDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
