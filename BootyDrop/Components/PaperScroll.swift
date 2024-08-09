6dd261cd6fb6c02bd30141f50f9ee9e34f9103a1
Kevin Green <stuckbykev@gmail.com>
2024-08-09 15:41:06 -0700

    Finished the settings view along with a new VIew component; PaperScroll, and handled the presentation aspects. Just needs new/updated button images for music, sound, vibrate, and restart buttons.

---
+++ BootyDrop/Components/PaperScroll.swift
@@ -0,0 +1,148 @@
+//
+//  PaperScroll.swift
+//  BootyDrop
+//
+//  Created by Kevin Green on 8/9/24.
+//
+
+import SwiftUI
+
+struct PaperScroll<Content: View>: View {
+    @Binding var show: Bool { willSet { animatePaperScroll() } }
+    var height: CGFloat = 400
+    @ViewBuilder var content: Content
+    
+    @State private var scrollTopOffset: CGFloat = -36
+    @State private var scrollBottomOffset: CGFloat = 36
+    @State private var scrollMiddleHeight: CGFloat = 30
+    @State private var scrollMiddleOffset: CGFloat = 0
+    @GestureState var dragAmount = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            Image("scroll_middle")
+                .resizable()
+                .overlay(alignment: .top) {
+                    content.padding(.top, 24)
+                } // Main Content
+                .frame(width: 310, height: scrollMiddleHeight)
+                .offset(y: scrollMiddleOffset)
+                .clipped()
+            
+            Image("scroll_bottom")
+                .resizable()
+                .scaledToFit()
+                .offset(y: scrollBottomOffset)
+                .offset(dragAmount)
+                .gesture(dragToClose)
+                .gesture(tapToClose.simultaneously(with: dragToClose))
+            
+            Image("scroll_top")
+                .resizable()
+                .scaledToFit()
+                .offset(y: scrollTopOffset)
+            
+        } // end paper scroll
+        .frame(width: 360)
+        .transition(.scale)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now()+0.3) {
+                animatePaperScroll()
+            }
+        }
+    }
+    
+    var dragToClose: some Gesture {
+        return DragGesture()
+            .onChanged { value in
+                // only allow drag after the paper scroll is opened.
+                if value.location.y > value.startLocation.y && value.translation.height <= 70 {
+                    // adjusts the middle scroll's height to stretch with the drag.
+                    let distance = value.translation.height - dragAmount.height
+                    scrollMiddleHeight += distance*2
+                    scrollMiddleOffset += distance
+                }
+            }
+            .updating($dragAmount) { value, state, transaction in
+                // only allow drag after the paper scroll is opened.
+                if scrollMiddleHeight >= height {
+                    // keep the draggable area within a range.
+                    if value.location.y > value.startLocation.y && value.translation.height <= 70 {
+                        state.height = value.translation.height
+                    }
+                }
+            }
+            .onEnded { value in
+                scrollMiddleHeight = height // resets the height
+                scrollMiddleOffset = 0 // resets the offset
+                DispatchQueue.main.asyncAfter(deadline: .now()+0.3) {
+                    animatePaperScroll()
+                }
+            }
+    }
+    
+    var tapToClose: some Gesture {
+        TapGesture(count: 1)
+            .onEnded { value in
+                animatePaperScroll()
+            }
+    }
+    
+    func animatePaperScroll() {
+        withAnimation(.easeInOut) {
+            scrollTopOffset == -36 ? (scrollTopOffset = (-height/2)-18 )  : (scrollTopOffset = -36)
+            scrollBottomOffset == 36 ? (scrollBottomOffset = (height/2)+18 ) : (scrollBottomOffset = 36)
+            scrollMiddleHeight == 30 ? (scrollMiddleHeight = height) : (scrollMiddleHeight = 30)
+        }
+}
+    
+}
+
+
+#Preview {
+    @State var showSettings: Bool = true
+    
+    return PaperScroll(show: $showSettings) {
+        VStack {
+            Text("Settings")
+                .font(.custom(CustomFont.rum, size: 30, relativeTo: .largeTitle))
+                .pirateShadow()
+            
+            VStack(spacing: 18) {
+                Button(action: {
+                    
+                }, label: {
+                    ButtonLabel(imageName: "trophy", title: "Music")
+                })
+                
+                Button(action: {}, label: {
+                    ButtonLabel(imageName: "trophy", title: "Sound")
+                })
+                
+                Button(action: {}, label: {
+                    ButtonLabel(imageName: "trophy", title: "Vibrate")
+                })
+                
+            }.padding(.bottom, 20)
+            
+            Button(action: {}, label: {
+                ButtonLabel(imageName: "trophy", title: "Restart")
+            })
+            .buttonStyle(.borderedProminent)
+        }
+    }
+    
+    func ButtonLabel(imageName: String, title: String, frame: CGSize? = nil) -> some View {
+        VStack {
+            Image(imageName)
+                .resizable()
+                .frame(width: frame?.width ?? 20, height: frame?.height ?? 20)
+            Text(title)
+                .font(.custom(CustomFont.rum, size: 16, relativeTo: .subheadline))
+                .foregroundStyle(Color.orange.gradient)
+                .pirateShadow(y: 4)
+            
+        }
+    }
+    
+}
---
+++ BootyDrop/Image Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Image Extensions.swift
+//  BootyDrop
+//
+//  Created by Kevin Green on 8/9/24.
+//
+
+import SwiftUI
+
+struct Image_Extensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Image_Extensions()
+}
