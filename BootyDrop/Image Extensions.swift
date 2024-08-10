b93101ba7e40f7cc00bd1ced22759f0b5f18b69d
Kevin Green <46732563+kevingreen22@users.noreply.github.com>
2024-08-09 17:02:24 -0700

    Update Image Extensions.swift

--- BootyDrop/Image Extensions.swift
+++ BootyDrop/Image Extensions.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct Image_Extensions: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+extension Image {
+    func flipHorizontal() -> some View {
+        self.rotation3DEffect(.degrees(180), axis: (x: 1.0, y: 0.0, z: 0.0))
     }
-}
-
-#Preview {
-    Image_Extensions()
+    
+    func flipVertically() -> some View {
+        self.rotation3DEffect(.degrees(180), axis: (x: 0.0, y: 1.0, z: 0.0))
+    }
 }
