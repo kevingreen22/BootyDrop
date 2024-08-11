2e2afbe41a89f105768ea24a2471e220a5238572
Kevin Green <stuckbykev@gmail.com>
2024-08-10 17:22:16 -0700

    Created rankings view

---
+++ BootyDrop/RankingsView.swift
@@ -0,0 +1,18 @@
+//
+//  RankingsView.swift
+//  BootyDrop
+//
+//  Created by Kevin Green on 8/10/24.
+//
+
+import SwiftUI
+
+struct RankingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RankingsView()
+}
