f8b5c906538672ae2ec124d167182c17bc3911ee
Kevin Green <stuckbykev@gmail.com>
2024-08-13 15:04:13 -0700

    Refactored some GC manager methods, cleaned up LeaderboardView

--- BootyDrop/GameCenterManager.swift
+++ BootyDrop/GameCenterManager.swift
@@ -11,28 +11,14 @@ import SwiftUI
 import GameKit
 import Combine
 
-//    .onAppear() {
-//        if !GKLocalPlayer.local.isAuthenticated {
-//            authenticateUser()
-//        } else if playersList.count == 0 {
-//            Task {
-//                await loadLeaderboard()
-//            }
-//        }
-//    }
-
 class GameCenterManager {
     private let leaderboardIdentifier = "highscores_1234567890"
-    
     let shared = GameCenterManager()
-    
     private init() {}
-    
-    var playersList: [GKPlayer] = []
-    
-    
+        
     class func authenticateUser() {
-        GKLocalPlayer.local.authenticateHandler = { vc, error in
+        let localPlayer = GKLocalPlayer.local
+        localPlayer.authenticateHandler = { vc, error in
             guard error == nil else {
                 print(error?.localizedDescription ?? "")
                 return
@@ -43,47 +29,27 @@ class GameCenterManager {
                 return
             }
             print("Authenticated as \(GKLocalPlayer.local.displayName)")
+            GKAccessPoint.shared.location = .topLeading
+            GKAccessPoint.shared.isActive = localPlayer.isAuthenticated
         }
     }
     
-    class func fetchLeaderboard() async throws -> [GKLeaderboard.Entry] {
+    class func fetchLeaderboard() async throws -> (GKLeaderboard.Entry?, [GKLeaderboard.Entry], Int) {
         let leaderboard = try await GKLeaderboard.loadLeaderboards(IDs: ["highscores_1234567890"]).first
 
-        guard let scores = try await leaderboard?.loadEntries(for: .global, timeScope: .allTime, range: NSRange(1...10)) else { return [] }
+        guard let scores = try await leaderboard?.loadEntries(for: .global, timeScope: .allTime, range: NSRange(1...10)) else { return (nil,[],0) }
 
-        for entry in scores.1 {
-            let player = entry.player
-            let score = entry.formattedScore
-            let image = try? await entry.player.loadPhoto(for: .small)
-
-            print("\(player.displayName) - \(score)")
-        }
-        
-        return scores.1
-    }
-    
-//    func loadLeaderboard() async {
-//        playersList.removeAll()
-//        Task {
-//            var playersListTemp : [GKPlayer] = []
-//            let leaderboards = try await GKLeaderboard.loadLeaderboards(IDs: [leaderboardIdentifier])
-//            if let leaderboard = leaderboards.filter ({ $0.baseLeaderboardID == self.leaderboardIdentifier }).first {
-//                let allPlayers = try await leaderboard.loadEntries(for: .global, timeScope: .allTime, range: NSRange(1...10))
-//                if allPlayers.1.count > 0 {
-//                    try await allPlayers.1.forEach { leaderboardEntry in
-//                        var image = try await leaderboardEntry.player.loadPhoto(for: GKPlayer.PhotoSize.small)
-//                        playersListTemp.append(GKPlayer(name: leaderboardEntry.player.displayName, score:leaderboardEntry.formattedScore, image: image))
-//                        print(playersListTemp)
-//                        playersListTemp.sort{
-//                            $0.score < $1.score
-//                        }
-//                    }
-//                }
-//            }
-//            playersList = playersListTemp
+//        for entry in scores.1 {
+//            let player = entry.player
+//            let score = entry.formattedScore
+//            let image = try? await entry.player.loadPhoto(for: .small)
+//
+//            print("\(player.displayName) - \(score)")
 //        }
-//    }
-    
+        
+        return scores
+    }
+        
     class func submit(_ highScore: Int, context: Int = 0, player: GKLocalPlayer = .local, leaderboardIDs: [String] = ["highscores_1234567890"]) async throws {
         try await GKLeaderboard.submitScore(
             highScore,
@@ -92,7 +58,6 @@ class GameCenterManager {
             leaderboardIDs: leaderboardIDs
         )
     }
-    
     
     class func submit(_ achievementID: AchievementID, percentComplete: Double = 100) async throws {
         let achievement = GKAchievement(identifier: achievementID.rawValue)
@@ -106,7 +71,7 @@ class GameCenterManager {
         }
     }
     
-    enum AchievementID: String {
+    public enum AchievementID: String {
         case firstWin = "first_win"
     }
     
@@ -152,25 +117,3 @@ public struct GameCenterView: UIViewControllerRepresentable {
 
 
 
-
-
-struct LeaderboardView: View {
-    @State private var leaderboardEntries: [GKLeaderboard.Entry] = []
-    
-    var body: some View {
-        List(leaderboardEntries, id: \.player.gamePlayerID) { entry in
-            HStack {
-                Text("\(entry.player.displayName)")
-                Spacer()
-                Text("\(entry.formattedScore)")
-            }
-        }
-        .task {
-            do {
-                leaderboardEntries = try await GameCenterManager.fetchLeaderboard()
-            } catch {
-                print("Failed to fetch leaderboard: \(error)")
-            }
-        }
-    }
-}
