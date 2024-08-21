a884dce47990feec9cfc4a3d245d65a9ffb0b8cc
Kevin Green <stuckbykev@gmail.com>
2024-08-21 15:43:44 -0700

    Added new images to image folder/assetes, refactored some more, added more button configurations, and continued building out secondary views.

---
+++ BootyDrop/Helper Files/AppStorageKey.swift
@@ -0,0 +1,14 @@
+//
+//  AppStorageKeys.swift
+//  BootyDrop
+//
+//  Created by Kevin Green on 8/21/24.
+//
+
+import SwiftUI
+
+struct AppStorageKeys {
+    static var music = "music"
+    static var sound = "sound"
+    static var vibrate = "vibrate"
+}
